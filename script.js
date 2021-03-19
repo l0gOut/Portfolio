e633a89aa3eca53f7c2db950e32baac00f11b9ad
Daniil Petrunin <Ne skajy>
2021-03-19 03:16:49 +0300

    update

--- script.js
+++ script.js
@@ -2,24 +2,34 @@ const background = document.querySelector("[data-background]")
 const flashcardsPopOut = document.querySelector("[data-div-flashcards]")
 const virtualPianoPopOut = document.querySelector("[data-div-virtual-piano]")
 const buttonFlashcard = document.getElementById("flashcards_button");
+const buttonPiano = document.getElementById("piano_button");
 const buttonExit = document.querySelectorAll("[data-button-exit]");
 
-buttonFlashcard.addEventListener("click", ev => {
-    background.classList.add("div_active");
-    flashcardsPopOut.classList.add("div_active");
+buttonFlashcard.addEventListener("click", () => {
+    open(flashcardsPopOut);
+});
+
+buttonPiano.addEventListener("click", () => {
+    open(virtualPianoPopOut);
 });
 
 buttonExit.forEach(value => {
-    value.addEventListener("click", evt => {
+    value.addEventListener("click", () => {
         close();
     });
 });
 
-background.addEventListener("click", evt => {
+background.addEventListener("click", () => {
     close();
 });
+
+function open(model) {
+    background.classList.add("div_active");
+    model.classList.add("div_active");
+}
 
 function close() {
     background.classList.remove("div_active");
     flashcardsPopOut.classList.remove("div_active");
+    virtualPianoPopOut.classList.remove("div_active")
 }
